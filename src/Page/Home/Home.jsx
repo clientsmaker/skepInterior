f2a2e067c1d31c4c79e855c570ba88c363f6d26d
clientsmaker <interiorclientsmaker@gmail.com>
2024-05-22 10:51:36 +0530

    modify about testimonial and features content

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -97,12 +97,12 @@ const Home = () => {
       </p>
 
       <div className="mt-8 flex flex-wrap gap-4 text-center">
-        <a
+        {/* <a
           href="#"
           className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
         >
           Get Started
-        </a>
+        </a> */}
 
         {/* <a
           href="#"
